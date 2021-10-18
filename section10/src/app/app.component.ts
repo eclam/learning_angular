23b8bce170ac859ae7360d3933ae5ebe8f718635
eclam <eclam@sfu.ca>
2021-10-18 15:49:35 -0700

    section 10: start on creating services

--- section10/src/app/app.component.ts
+++ section10/src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { ShoppingListService } from './shopping-list.service';
 import { Component } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:['ShoppingListService', 'RecipeService']
 })
 export class AppComponent {
   loadedFeature = 'recipe';
+
+  constructor(private recipeService: RecipeService,
+    private shoppingListService: ShoppingListService){}
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
