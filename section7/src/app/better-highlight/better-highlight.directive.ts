acc2d2b42fe7e26ae7734ebdbbe0717cdfb612b8
eclam <eclam@sfu.ca>
2021-10-18 09:35:24 -0700

    sect7 learn about hostlistener

--- section7/src/app/better-highlight/better-highlight.directive.ts
+++ section7/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,6 +9,15 @@ export class BetterHighlightDirective implements OnInit {
   // works with service workers -> might not have access to DOM
   constructor(private elRef: ElementRef, private renderer: Renderer2) { } // better if u cannot access Dom
   ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
+
 }
