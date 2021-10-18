4b3e1172e867441178defb335252ca677dc6686f
eclam <eclam@sfu.ca>
2021-10-18 09:49:21 -0700

    sect7 learn more about hostbinding and diff ways on setting bindings

--- section7/src/app/better-highlight/better-highlight.directive.ts
+++ section7/src/app/better-highlight/better-highlight.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2 } from '@angular/core';
 
@@ -10,22 +11,26 @@ import {
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @Input() defaultColor: string = 'transprent';
+  // @Input() highlightColor: string = 'blue';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
   // Not limited to running in the browser
   // works with service workers -> might not have access to DOM
   constructor(private elRef: ElementRef, private renderer: Renderer2) { } // better if u cannot access Dom
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 
