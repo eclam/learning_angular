a33c8ee31335564310689479c42dc2bd9f951ecb
eclam <eclam@sfu.ca>
2021-10-22 09:15:42 -0700

    section 11: learn about routers and dynamic routing

--- section11/src/app/users/user/user.component.ts
+++ section11/src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
-
+  paramsSubscription: Subscription;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -16,13 +17,20 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
       );
+  }
+
+  // Don't really need to do this b/c
+  //    angular does it behind the scenes already
+  // Just good to know about this.
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
