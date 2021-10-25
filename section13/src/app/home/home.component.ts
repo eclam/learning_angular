abbf58d8c74cf806eedcd52f639bd61a8b6c1fe4
eclam <eclam@sfu.ca>
2021-10-24 17:46:37 -0700

    section13: learn about pipe and map

--- section13/src/app/home/home.component.ts
+++ section13/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 // import { interval, Subscription } from 'rxjs';
 import { Subscription, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -28,7 +29,15 @@ export class HomeComponent implements OnInit, OnDestroy {
         count++;
       }, 1000)
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+
+    this.firstObsSubscription = customIntervalObservable.pipe(
+      // filter(
+
+      // ),
+      map(
+        (data:number) => {
+          return 'Round: ' + (data + 1);
+    })).subscribe((data) => {
       console.log(data);
     }, (error)=> {
       console.log(error);
