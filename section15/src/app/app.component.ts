23826a4fc833a42b1e667cc6bce1ecfeede2e1ec
eclam <eclam@sfu.ca>
2021-10-27 10:17:37 -0700

    section15: learn about set and patch value of forms

--- section15/src/app/app.component.ts
+++ section15/src/app/app.component.ts
@@ -10,9 +10,24 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female', 'other'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender:'male'
+    // })
+    this.signupForm.form.patchValue({
+      userData: {
+        username:suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm){
