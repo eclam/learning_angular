e48e41547b1bd73b5c0c4cbfc98feb627c6fc165
eclam <eclam@sfu.ca>
2021-10-29 13:37:52 -0700

    sect15 pt2: learn about setting, patching and resetting reactive forms

--- section15_pt2_reactive_approach/src/app/app.component.ts
+++ section15_pt2_reactive_approach/src/app/app.component.ts
@@ -21,10 +21,35 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value)=>{
+    //     console.log(value)
+    //   }
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (status) => {
+        console.log(status)
+      }
+    );
+    // this.signupForm.setValue({
+    //   'userData':{
+    //     'username':'Max',
+    //     'email':'max@test.com'
+    //   },
+    //   'gender':'male',
+    //   'hobbies':[]
+    // });
+    // this.signupForm.patchValue({
+    //   'userData': {
+    //     'username': 'Anna',
+    //     'email': 'max@test.com'
+    //   }
+    // });
   }
 
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby(){
@@ -40,7 +65,8 @@ export class AppComponent implements OnInit {
     if (this.forbiddenUsernames.indexOf(control.value)) {
       return {'nameIsForbidden':true};
     }
-    return null; // Should not return anything or it'll treat it as something is returned
+    return null;
+    // Should not return anything or it'll treat it as something is returned
   }
 
   forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
